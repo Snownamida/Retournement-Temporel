c2f2372543c250e2daaadc3d82b70d3d28a91e63
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2023-04-28 13:52:51 +0200

    优化渲染算法

--- wave_render.py
+++ wave_render.py
@@ -10,21 +10,32 @@ u = np.load("./wave/" + para_string + ".npz")["u"]
 
 fps = 40
 render_time = T  # temps de rendu
-render_speed = 0.1
+render_speed = 0.15
 # 1 seconde du temps réel correspond à combien seconde du temps de rendu
 
 N_frame = int(fps * render_time / render_speed)
 
 fig, ax = plt.subplots(figsize=(16, 9))
+ax.set_xlim([0, Lx])
+ax.set_ylim([0, Ly])
+ax.set_xlabel("x")
+ax.set_ylabel("y")
 u_max = 0.1
 
 print("rendering...")
 t0 = time.time()
 
 
+u_img = ax.imshow([[]], cmap="seismic", vmin=-u_max, vmax=u_max, extent=[0, Lx, 0, Ly],zorder=0)
+coeur_img = ax.scatter([], [], c="r", s=1,zorder=5)
+
+
 def animate(n_frame):
     global t0
     n = int(render_speed / dt / fps * n_frame)
+    ax.set_title(f"t={n*dt:.5f}")
+    u_img.set_data(u[n, :, ::-1].T)
+    coeur_img.set_offsets(np.argwhere(coeur) * dl)
     if not n_frame % 10:
         t1 = time.time()
         print(
@@ -33,20 +44,10 @@ def animate(n_frame):
             flush=True,
         )
         t0 = t1
-    ax.clear()
-    ax.set_xlim([0, Lx])
-    ax.set_ylim([0, Ly])
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.set_title(f"t={n*dt:.5f}")
-    u_img = ax.imshow(
-        u[n, :, ::-1].T, cmap="coolwarm", vmin=-u_max, vmax=u_max, extent=[0, Lx, 0, Ly]
-    )
-    coeur_img=ax.scatter(*(np.argwhere(coeur).T*dl),c="r",s=1)
 
     return u_img, coeur_img
 
 
 anim = animation.FuncAnimation(fig, animate, frames=N_frame, interval=50, blit=True)
-anim.save("./wave/" + para_string + ".mp4", writer="ffmpeg", fps=60)
+anim.save("./wave/" + para_string + ".mp4", writer="ffmpeg", fps=fps)
 print("\ndone")
